2351cf640d4ef674239b36bd07b3c661250e6f05
aimgodorganization <83001683+aimgodorganization@users.noreply.github.com>
2021-05-01 19:09:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ client.snipes = new Discord.Collection();
 });
 
 client.login(process.env.TOKEN);
+
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Your Bot has Started Working! Do join our Discord Server and Subscribe to our Channel for more Such Tutorials!');
+});
+server.listen(3000);
